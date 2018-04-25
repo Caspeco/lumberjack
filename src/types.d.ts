3f7ef7c6f67f7f3dc32c5e0c0d96c71a48316e11
Anders Åberg <anders@andersaberg.com>
2018-04-25 11:11:47 +0200

    Removed stuff from gh-apges

--- src/types.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'moment-json-parser';
-declare module 'react-string-replace'; 
-declare module '@researchgate/react-intersection-list';
-declare module 'worker-loader!./worker.js';
-declare module 'transit-immutable-js';
