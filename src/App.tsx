ba84a76c659772c4665f00b5e913c993ad0bdf32
Anders Åberg <anders@andersaberg.com>
2018-07-09 16:05:03 +0200

    Did more refactoring.

--- src/App.tsx
+++ src/App.tsx
@@ -30,13 +30,13 @@ import BadRequestError from "./badrequesterror";
 import SearchString from "search-string";
 
 
-import {ConsoleRow, ILogRow} from "./ConsoleRow";
-import {ConsoleView} from "./ConsoleView";
-import {worker1} from "./workerWrap";
+import { ConsoleRow, ILogRow } from "./ConsoleRow";
+import { ConsoleView } from "./ConsoleView";
+import { worker1 } from "./workerWrap";
 
-import {DetailsView} from "./DetailsView";
-import {TimeChart} from "./TimeChart";
-import {logContainer, LogContainer} from "./logContainer"
+import { DetailsView } from "./DetailsView";
+import { TimeChart } from "./TimeChart";
+import { logContainer, LogContainer } from "./logContainer"
 
 const API_BASE = "https://api.applicationinsights.io/v1/apps/";
 
@@ -219,14 +219,13 @@ const keyBindingsMap = {
 };
 
 export class App extends React.Component<{}, IState> {
-  
-  /**
-   *
-   */
+
+  private refreshTimerId: NodeJS.Timer;
+
   constructor(props: {}) {
     super(props);
 
-    var existingQuery = {
+    const existingQuery = {
       ...momentjson(localStorage.getItem("query") as string)
     };
     if (existingQuery && existingQuery.timeRange) {
@@ -317,14 +316,7 @@ export class App extends React.Component<{}, IState> {
     })
   }
 
-  private setAgo = (time: string) => {
-    const amount = parseInt(time.substr(0,2))
-    const unit = time[2];
-    this.handleTimeRangeChange(
-      this.state.query.timeRange.from = moment().subtract(amount as any, unit),
-      null as any as moment.Moment
-    );
-  }
+
 
   public async componentDidMount() {
     this.getData();
@@ -345,7 +337,7 @@ export class App extends React.Component<{}, IState> {
         onChange={this.handleChangeOrderBy}
         value={this.state.query.orderBy}
       >
-        <Option value="desc"  title={"Desc"}>
+        <Option value="desc" title={"Desc"}>
           <Icon type="arrow-down" />
         </Option>
         <Option value="asc" title={"Asc"}>
@@ -387,41 +379,43 @@ export class App extends React.Component<{}, IState> {
             <HotKeys keyMap={keyBindingsMap} handlers={handlers}>
               <div className="App">
                 <header className="App-header">
-                <div className="topline">
-                  <div className="searchBar">
-                    <Input.Search
-                      placeholder="Grep for message..."
-                      type="search"
-                      value={this.state.query.grep}
-                      onChange={this.handleSearchChange}
-                      addonAfter={orderBy}
-                    />
+                  <div className="topline">
+                    <div className="searchBar">
+                      <Input.Search
+                        placeholder="Grep for message..."
+                        type="search"
+                        value={this.state.query.grep}
+                        onChange={this.handleSearchChange}
+                        addonAfter={orderBy}
+                      />
                     </div>
                     <TreeSelect {...tProps} />
                     <Checkbox
                       checked={this.state.autoRefresh}
                       className="leftMargin"
                       onChange={this.handleAutoRefreshChange}>Auto refresh</Checkbox>
-                      
-                    <Button onClick={this.handleShowSettings} style={{marginLeft:"10px"}}>Settings</Button>
+
+                    <Button onClick={this.handleShowSettings} style={{ marginLeft: "10px" }}>Settings</Button>
                   </div>
-                  
+
                   <div className="searchControls">
                     <Button.Group size="small">
-                    <Button onClick={() => this.setAgo("30m")}>Last 30m</Button>
-                    <Button onClick={() => this.setAgo("01h")}>Last 60m</Button>
-                    <Button onClick={() => this.setAgo("02h")}>Last 2h</Button>
-                    <Button onClick={() => this.setAgo("12h")}>Last 12h</Button>
-                    <Button onClick={() => this.setAgo("24h")}>Last 24h</Button>
-                    <Button onClick={() => this.setAgo("02d")}>Last 2d</Button>
-                    <Button onClick={() => this.setAgo("07d")}>Last week</Button>
+                      {/* tslint:disable:jsx-no-lambda*/}
+                      <Button onClick={() => this.setAgo("30m")}>Last 30m</Button>
+                      <Button onClick={() => this.setAgo("01h")}>Last 60m</Button>
+                      <Button onClick={() => this.setAgo("02h")}>Last 2h</Button>
+                      <Button onClick={() => this.setAgo("12h")}>Last 12h</Button>
+                      <Button onClick={() => this.setAgo("24h")}>Last 24h</Button>
+                      <Button onClick={() => this.setAgo("02d")}>Last 2d</Button>
+                      <Button onClick={() => this.setAgo("07d")}>Last week</Button>
+                      {/* tslint:enable:jsx-no-lambda*/}
                     </Button.Group>
                     <RangePicker
                       size="small"
                       className="timePicker"
                       defaultValue={[this.state.query.timeRange.from, to]}
                       style={{
-                        marginLeft:"10px"
+                        marginLeft: "10px"
                       }}
                       ranges={{
                         "Last 30m": [
@@ -453,7 +447,7 @@ export class App extends React.Component<{}, IState> {
                             .endOf("month")
                         ]
                       }}
-                      
+
                       value={currentTimeRangeValue}
                       showTime
                       format="YYYY-MM-DD HH:mm:ss"
@@ -487,14 +481,14 @@ export class App extends React.Component<{}, IState> {
                   title="Details"
                   visible={this.state.showDetails !== null}
                   width="90%"
-                  onCancel={() => this.setState({ showDetails: null })}
-                  onOk={() => this.setState({ showDetails: null })}
+                  onCancel={this.hideDetails}
+                  onOk={this.hideDetails}
                 >
                   {this.state.showDetails === null ? null :
-                    <DetailsView details={this.state.showDetails}/>
+                    <DetailsView details={this.state.showDetails} />
                     // <div dangerouslySetInnerHTML=
                     //   {{__html: jsonMarkup(this.state.showDetails)}}>
-                      
+
                     // </div>
                   }
                 </Modal>
@@ -525,20 +519,27 @@ export class App extends React.Component<{}, IState> {
                   <Divider>Add New</Divider>
                   <Input
                     value={this.state.settings.newApp.appId}
+                    // tslint:disable-next-line:jsx-no-lambda
                     onChange={x => this.newApp("appId", x)}
                     addonBefore="App ID"
                   />
                   <Input
                     value={this.state.settings.newApp.apiKey}
+                    // tslint:disable-next-line:jsx-no-lambda
                     onChange={x => this.newApp("apiKey", x)}
                     addonBefore="Api Key"
                   />
                   <Input
                     value={this.state.settings.newApp.name}
+                    // tslint:disable-next-line:jsx-no-lambda
                     onChange={x => this.newApp("name", x)}
                     addonBefore="Name"
                   />
-                  <Button type="primary" onClick={e => this.newApp("add", e)}>
+
+                  <Button
+                    type="primary"
+                    // tslint:disable-next-line:jsx-no-lambda
+                    onClick={e => this.newApp("add", e)}>
                     Add new App
                   </Button>
                 </Modal>
@@ -547,6 +548,18 @@ export class App extends React.Component<{}, IState> {
           )}
         </Subscribe>
       </Provider>
+    );
+  }
+
+  private hideDetails = () => {
+    this.setState({ showDetails: null });
+  }
+  private setAgo = (time: string) => {
+    const amount = parseInt(time.substr(0, 2), 10)
+    const unit = time[2];
+    this.handleTimeRangeChange(
+      this.state.query.timeRange.from = moment().subtract(amount as any, unit),
+      null as any as moment.Moment
     );
   }
 
@@ -588,7 +601,7 @@ export class App extends React.Component<{}, IState> {
     this.setState({});
   };
 
-  private refreshTimerId: NodeJS.Timer;
+
   private handleAutoRefreshChange = (e: any) => {
     //  CheckboxChangeEvent
     this.setState({
@@ -596,13 +609,13 @@ export class App extends React.Component<{}, IState> {
     });
 
     clearInterval(this.refreshTimerId);
-    
+
     const refreshRate = 60 * 1000; // every 1 min
-    if(e.target.checked) {
+    if (e.target.checked) {
       this.getData();
       this.refreshTimerId = setInterval(() => {
         this.getData();
-      }, refreshRate );
+      }, refreshRate);
     }
   }
 
@@ -614,7 +627,7 @@ export class App extends React.Component<{}, IState> {
       "to",
       this.state.currentQuery - 1
     );
-    var last = this.state.queryHistory.get(current - 1);
+    const last = this.state.queryHistory.get(current - 1);
     if (!last) {
       return;
     }
@@ -627,9 +640,9 @@ export class App extends React.Component<{}, IState> {
   };
 
   private goForward = () => {
-    var current = this.state.currentQuery;
+    const current = this.state.currentQuery;
     console.log("go forward from", current, "to", current + 1);
-    var last = this.state.queryHistory.get(current + 1);
+    const last = this.state.queryHistory.get(current + 1);
     if (!last) {
       return;
     }
@@ -731,13 +744,13 @@ export class App extends React.Component<{}, IState> {
   private showDetailedError(error: any) {
     console.error("Detailed error", error);
 
-    let title = error.message;
-    let message = ""
+    const title = error.message;
+    let messageText = ""
     let innerTitle = ""
     let innerMessage = ""
     if (error.code === "PartialError") {
       const ed0 = error.details[0];
-      message = ed0.message;
+      messageText = ed0.message;
       innerTitle = ed0.innererror.message;
     } else {
       // message = error.
@@ -750,19 +763,20 @@ export class App extends React.Component<{}, IState> {
       <Button
         type="primary"
         size="small"
+        // tslint:disable-next-line:jsx-no-lambda
         onClick={() => notification.close(key)}
       >
         Close
           </Button>
     );
     notification.error({
-      key: key,
-      btn: btn,
+      key,
+      btn,
       duration: 20,
       message: "Error: " + title,
       description: (
         <div>
-          <p>{message}</p>
+          <p>{messageText}</p>
           <strong>{innerTitle}</strong>
           <p>{innerMessage}</p>
           <p>See console for more details</p>
@@ -782,7 +796,7 @@ export class App extends React.Component<{}, IState> {
     // });
 
     // var url = location.href;               //Save down the URL without hash.
-    location.href = "#" + r.get("itemId");                 //Go to the target element.
+    location.href = "#" + r.get("itemId");                 // Go to the target element.
     // history.replaceState(null,null as any,url);   //Don't like hashes. Changing it back.
     console.log(r, r.toJS());
     this.setState(() => {
@@ -822,15 +836,15 @@ export class App extends React.Component<{}, IState> {
       const apiKey = this.state.settings.currentApp.apiKey || "";
       const baseSettings = {
         url: API_BASE + appId + "/query",
-        apiKey: apiKey
+        apiKey
       };
 
-      var graphRes = worker1.emit("fetchGraphData", {
+      const graphRes = worker1.emit("fetchGraphData", {
         ...baseSettings,
         query: queries.graphQuery
       });
 
-      let logRes = worker1.emit("start-fetchLogData", {
+      const logRes = worker1.emit("start-fetchLogData", {
         ...baseSettings,
         query: queries.logQuery
       });
@@ -850,7 +864,7 @@ export class App extends React.Component<{}, IState> {
 
       // do not warn on manual abort
       if (error.badRequest) {
-        let data = await error.response;
+        const data = await error.response;
         console.log("bad data", data);
         this.showDetailedError(data.error);
       } else if (error.name !== "AbortError" && error.code !== 20) {
